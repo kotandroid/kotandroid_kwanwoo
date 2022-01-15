a3946e06f8aa256ee01a99529ed5e2601934afbd
Kim-kwan-woo <kw2577@naver.com>
2022-01-15 21:47:48 +0900

    chapter 9 challenge

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -55,9 +56,15 @@ class CrimeListFragment : Fragment() {
 
         private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
         private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        private val policeButton: Button? = itemView.findViewById(R.id.call_button) ?: null //경찰에 연락 버튼(챌린지9)
 
         init {
             itemView.setOnClickListener(this)
+            //경찰에 연락 버튼 기능(챌린지 9)
+            policeButton?.setOnClickListener {
+                Toast.makeText(context, "경찰에 연락했습니다!", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
         fun bind(crime: Crime) {
@@ -69,14 +76,20 @@ class CrimeListFragment : Fragment() {
         override fun onClick(v: View?) {
             Toast.makeText(context, "${crime.title} pressed!!", Toast.LENGTH_SHORT).show()
         }
+
     }
 
     private inner class CrimeAdapter(var crimes: List<Crime>)
         : RecyclerView.Adapter<CrimeHolder>() {
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
-            val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
-            return CrimeHolder(view)
+            return if (viewType == 0) { //경찰에 연락 버튼이 없는 뷰
+                val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
+                CrimeHolder(view)
+            } else { //경찰에 연락이 있는 뷰
+                val view = layoutInflater.inflate(R.layout.list_item_crime_require_police, parent, false)
+                CrimeHolder(view)
+            }
         }
 
         override fun getItemCount() = crimes.size
@@ -84,6 +97,11 @@ class CrimeListFragment : Fragment() {
         override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
             val crime = crimes[position]
             holder.bind(crime)
+        }
+        //뷰 타입 리턴(챌린지 9)
+        override fun getItemViewType(position: Int): Int {
+            val crime = crimes[position]
+            return crime.requiresPolice
         }
     }
 
