41a87b702096ded9c7e90ba0d7bba7d728c5979d
Kim-kwan-woo <kw2577@naver.com>
2022-02-11 22:47:22 +0900

    chapter 20 basic

--- BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
@@ -1,18 +1,35 @@
 package com.bignerdranch.android.beatbox
 
+import android.content.res.AssetFileDescriptor
 import android.content.res.AssetManager
+import android.media.SoundPool
 import android.util.Log
+import java.io.IOException
 import java.lang.Exception
 
 private const val TAG = "BeatBox"
 private const val SOUNDS_FOLDER = "sample_sounds"
+private const val MAX_SOUNDS = 5
 
 class BeatBox(private val assets: AssetManager) {
 
     val sounds: List<Sound>
+    private val soundPool = SoundPool.Builder()
+        .setMaxStreams(MAX_SOUNDS)
+        .build()
 
     init {
         sounds = loadSounds()
+    }
+
+    fun play(sound: Sound) {
+        sound.soundId?.let {
+            soundPool.play(it, 1.0f, 1.0f, 1, 0, 1.0f)
+        }
+    }
+
+    fun release() {
+        soundPool.release()
     }
 
     private fun loadSounds(): List<Sound> {
@@ -25,11 +42,22 @@ class BeatBox(private val assets: AssetManager) {
             return emptyList()
         }
         val sounds = mutableListOf<Sound>()
-        soundNames.forEach { fileName ->
-            val assetPath = "$SOUNDS_FOLDER/$fileName"
+        soundNames.forEach { filename ->
+            val assetPath = "$SOUNDS_FOLDER/$filename"
             val sound = Sound(assetPath)
-            sounds.add(sound)
+            try {
+                load(sound)
+                sounds.add(sound)
+            } catch (ioe: IOException) {
+                Log.e(TAG, "Cound not load sound $filename", ioe)
+            }
         }
         return sounds
+    }
+
+    private fun load(sound: Sound) {
+        val afd: AssetFileDescriptor = assets.openFd(sound.assetPath)
+        val soundId = soundPool.load(afd, 1)
+        sound.soundId = soundId
     }
 }
--- BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/Sound.kt
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/Sound.kt
@@ -2,7 +2,7 @@ package com.bignerdranch.android.beatbox
 
 private const val WAV = ".wav"
 
-class Sound(val assetPath: String) {
+class Sound(val assetPath: String, var soundId: Int? = null) {
 
     val name = assetPath.split("/").last().removeSuffix(WAV)
 }
