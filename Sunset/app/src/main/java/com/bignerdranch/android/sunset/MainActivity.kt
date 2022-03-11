145c3fb5cf9ef1f2f298eff5134c267c73ca06bf
Kim-kwan-woo <kw2577@naver.com>
2022-03-11 14:51:17 +0900

    chapter 31 challenge 3

--- Sunset/app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
+++ Sunset/app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
@@ -3,6 +3,7 @@ package com.bignerdranch.android.sunset
 import android.animation.AnimatorSet
 import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -14,14 +15,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var sceneView: View
     private lateinit var sunView: View
     private lateinit var skyView: View
+    private lateinit var animatorSet: AnimatorSet
     private var checkSun: Boolean = true
 
-    private val sunYStart: Float by lazy {
-        sunView.top.toFloat()
-    }
-    private val sunYEnd: Float by lazy {
-        skyView.height.toFloat()
-    }
+//    private val sunYStart: Float by lazy {
+//        sunView.top.toFloat()
+//    }
+//    private val sunYEnd: Float by lazy {
+//        skyView.height.toFloat()
+//    }
 
     private val blueSkyColor: Int by lazy {
         ContextCompat.getColor(this, R.color.blue_sky)
@@ -40,19 +42,16 @@ class MainActivity : AppCompatActivity() {
         sceneView = findViewById(R.id.scene)
         sunView = findViewById(R.id.sun)
         skyView = findViewById(R.id.sky)
+        animatorSet = AnimatorSet()
 
         sceneView.setOnClickListener {
-            if (checkSun) {
-                startAnimation()
-            } else {
-                startReverseAnimation()
-            }
+            startAnimation()
         }
     }
 
     private fun startAnimation() {
-        //val sunYStart = sunView.top.toFloat()
-        //val sunYEnd = skyView.height.toFloat()
+        val sunYStart = sunView.top.toFloat()
+        val sunYEnd = skyView.height.toFloat()
 
         val heightAnimator = ObjectAnimator
             .ofFloat(sunView, "y", sunYStart, sunYEnd)
@@ -69,39 +68,51 @@ class MainActivity : AppCompatActivity() {
             .setDuration(1500)
         nightSkyAnimator.setEvaluator(ArgbEvaluator())
 
-        val animatorSet = AnimatorSet()
         animatorSet.play(heightAnimator)
             .with(sunsetSkyAnimator)
             .before(nightSkyAnimator)
         animatorSet.start()
 
-        checkSun = false
+        if (checkSun) {
+            animatorSet.start()
+            checkSun = false
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val playTime = animatorSet.currentPlayTime
+                animatorSet.apply {
+                    pause()
+                    reverse()
+                    currentPlayTime = animatorSet.totalDuration - playTime
+                }
+            }
+            checkSun = true
+        }
     }
 
-    //chapter 31 challenge 1
-    private fun startReverseAnimation() {
-
-        val heightAnimator = ObjectAnimator
-            .ofFloat(sunView, "y", sunYEnd, sunYStart)
-            .setDuration(3000)
-        heightAnimator.interpolator = AccelerateInterpolator()
-
-        val sunsetSkyAnimator = ObjectAnimator
-            .ofInt(skyView, "backgroundColor", sunsetSkyColor, blueSkyColor)
-            .setDuration(3000)
-        sunsetSkyAnimator.setEvaluator(ArgbEvaluator())
-
-        val nightSkyAnimator = ObjectAnimator
-            .ofInt(skyView, "backgroundColor", nightSkyColor, sunsetSkyColor)
-            .setDuration(1500)
-        nightSkyAnimator.setEvaluator(ArgbEvaluator())
-
-        val animatorSet = AnimatorSet()
-        animatorSet.play(heightAnimator)
-            .with(sunsetSkyAnimator)
-            .after(nightSkyAnimator)
-        animatorSet.start()
-
-        checkSun = true
-    }
+    //chapter 31 challenge 1(이 방법 말고 reverse 사용하면 더 쉬움)
+//    private fun startReverseAnimation() {
+//
+//        val heightAnimator = ObjectAnimator
+//            .ofFloat(sunView, "y", sunYEnd, sunYStart)
+//            .setDuration(3000)
+//        heightAnimator.interpolator = AccelerateInterpolator()
+//
+//        val sunsetSkyAnimator = ObjectAnimator
+//            .ofInt(skyView, "backgroundColor", sunsetSkyColor, blueSkyColor)
+//            .setDuration(3000)
+//        sunsetSkyAnimator.setEvaluator(ArgbEvaluator())
+//
+//        val nightSkyAnimator = ObjectAnimator
+//            .ofInt(skyView, "backgroundColor", nightSkyColor, sunsetSkyColor)
+//            .setDuration(1500)
+//        nightSkyAnimator.setEvaluator(ArgbEvaluator())
+//
+//        val animatorSet = AnimatorSet()
+//        animatorSet.play(heightAnimator)
+//            .with(sunsetSkyAnimator)
+//            .after(nightSkyAnimator)
+//        animatorSet.start()
+//
+//        checkSun = true
+//    }
 }
